d2ccf694ca03ddf06312e86df3b5a14f12bac730
JANVI RAMANI <8804jaan@JANVIs-Air.home>
2021-02-16 00:02:04 -0500

    add

--- ScrollingApp/app/src/main/java/com/example/scrollingapp/MainActivity.java
+++ ScrollingApp/app/src/main/java/com/example/scrollingapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.scrollingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        TextView textView = (TextView) findViewById(R.id.textView);
+        String para = "This course surveys the specificities of the development of" +
+                " native applications for Android devices." +
+                " The software engineering " +
+                "of application development including user-centered design," +
+                " testing" +
+                " and quality assurance will be emphasized. " +
+                "Students will learn how" +
+                " to design and develop applications for the Android platform." +
+                " The " +
+                "following topics will be covered:" +
+                " intents, content providers, " +
+                "notifications, rich and responsible layouts, " +
+                "location based " +
+                "facilities, recycleviews, databases," +
+                " and network / web access. " +
+                "Kotlin will be covered. " +
+                "A project is integrated in the course.";
+        textView.setText(para);
+        textView.setMovementMethod(new ScrollingMovementMethod());
+    }}
